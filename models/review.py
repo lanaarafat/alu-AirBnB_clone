43a5013ee919391ff837079baadd020ccc545d1f
n-uwayo <n.uwayo@alustudent.com>
2023-09-12 07:03:29 -0700

    modules

---
+++ models/review.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module for review class
+"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review class Docs"""
+    place_id = ""
+    user_id = ""
+    text = ""
+

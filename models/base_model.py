be1110891715595c8bbf0fcf6fd83f763f5c6df7
n-uwayo <n.uwayo@alustudent.com>
2023-09-14 02:44:22 -0700

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""defining basemodel class"""
+"""Defining basemodel class"""
 import models
 from uuid import uuid4
 from datetime import datetime
@@ -9,35 +9,35 @@ class BaseModel:
     Class Base defines all common attributes or methods for other classes
     Attr:
     id: string - assign with a uuid when an instance is created
-    created_at: datetime - assign with the current datetime 
+    created_at: datetime - assign with the current datetime
     when an instance is created
-    updated_at: datetime - assign with the current datetime 
-    when an instance is created and it will be updated 
+    updated_at: datetime - assign with the current datetime
+    when an instance is created and it will be updated
     every time you change your object
     """
 
     def __init__(self, *args, **kwargs):
         """
-        new BaseModel
+        Create a new BaseModel instance
         """
         form = "%Y-%m-%dT%H:%M:%S.%f"
 
         if kwargs:
-          kwargs["created_at"] = datetime.strptime(
-          kwargs["created_at"], form)
-          kwargs["updated_at"] = datetime.strptime(
-          kwargs["updated_at"], form)
-          del kwargs["__class__"]
-          self.__dict__.update(kwargs)
+            kwargs["created_at"] = datetime.strptime(
+                kwargs["created_at"], form)
+            kwargs["updated_at"] = datetime.strptime(
+                kwargs["updated_at"], form)
+            del kwargs["__class__"]
+            self.__dict__.update(kwargs)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
 
     def save(self):
         """
-        updates the public instance attribute 
+        Update the public instance attribute
         updated_at with the current datetime
         """
         self.updated_at = datetime.now()
@@ -45,7 +45,7 @@ class BaseModel:
 
     def to_dict(self):
         """
-        returns a dictionary containing all
+        Return a dictionary containing all
         keys/values of __dict__ of the instance
         """
         new_dict = self.__dict__.copy()

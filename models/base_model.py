ec84e73d7e7564e12d36347ec7f50a2aaa65e3d1
n-uwayo <n.uwayo@alustudent.com>
2023-09-14 02:35:11 -0700

    basemode

--- models/base_model.py
+++ models/base_model.py
@@ -3,18 +3,19 @@
 import models
 from uuid import uuid4
 from datetime import datetime
+
 class BaseModel:
     """
     Class Base defines all common attributes or methods for other classes
     Attr:
-    id: string - assign with an uuid when an instance is created
+    id: string - assign with a uuid when an instance is created
     created_at: datetime - assign with the current datetime 
     when an instance is created
-    Updated_at: datetime - assign with the current datetime 
+    updated_at: datetime - assign with the current datetime 
     when an instance is created and it will be updated 
     every time you change your object
-
     """
+
     def __init__(self, *args, **kwargs):
         """
         new BaseModel
@@ -22,13 +23,12 @@ class BaseModel:
         form = "%Y-%m-%dT%H:%M:%S.%f"
 
         if kwargs:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], 
-							form)
-            kwargs["updated_at"] = datetime.strptime(
-                kwargs["updated_at"], form)
-            del kwargs["__class__"]
-            self.__dict__.update(kwargs)
-
+          kwargs["created_at"] = datetime.strptime(
+          kwargs["created_at"], form)
+          kwargs["updated_at"] = datetime.strptime(
+          kwargs["updated_at"], form)
+          del kwargs["__class__"]
+          self.__dict__.update(kwargs)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -44,10 +44,9 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self):
-
         """
         returns a dictionary containing all
-         keys/values of __dict__ of the instance
+        keys/values of __dict__ of the instance
         """
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__

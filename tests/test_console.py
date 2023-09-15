e4e9c936ca030bb8b2711c7832d42c6070c32e75
n-uwayo <n.uwayo@alustudent.com>
2023-09-15 03:52:49 -0700

    testconsole

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,62 +1,41 @@
 #!/usr/bin/python3
-"""tests for the console"""
-import unittest
-from io import StringIO
-from unittest.mock import patch
-import pep8
-import os
+"""
+Contains the class TestConsoleDocs
+"""
+
 import console
-import tests
-from console import HBNBCommand
+import inspect
+import pep8
+import unittest
+HBNBCommand = console.HBNBCommand
 
 
-class TestConsole(unittest.TestCase):
-    """tests for the console, quit and empty"""
+class TestConsoleDocs(unittest.TestCase):
+    """Class for testing documentation of the console"""
+    def test_pep8_conformance_console(self):
+        """Test that console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    @classmethod
-    def setUpClass(cls):
-        """setup console class for test"""
-        cls.consol = HBNBCommand()
+    def test_pep8_conformance_test_console(self):
+        """Test that tests/test_console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    @classmethod
-    def teardown(cls):
-        """tears down"""
-        del cls.consol
+    def test_console_module_docstring(self):
+        """Test for the console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
 
-    def tearDown(self):
-        """delete file"""
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
-    def test_pep8_console(self):
-        """Pep8 test"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(["console.py"])
-        self.assertEqual(p.total_errors, 0, 'fix Pep8')
-
-    def test_docstrings_in_console(self):
-        """check for doc strings"""
-        self.assertIsNotNone(console.__doc__)
-        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_show.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
-        self.assertIsNotNone(HBNBCommand.default.__doc__)
-
-    def test_emptyline(self):
-        """test for empty line"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd("test")
-            self.assertEqual("*** Unknown syntax: test\n", f.getvalue())
-
-    def test_quit(self):
-        """test the quit command works"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd("quit")
-            self.assertEqual('', f.getvalue())
+    def test_HBNBCommand_class_docstring(self):
+        """Test for the HBNBCommand class docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
